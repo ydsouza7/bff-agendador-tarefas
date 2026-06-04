ad911a1ba5a96da0d2354e35c255e6dd591ced66
Yan <eng.ydsouza@gmail.com>
2026-06-04 20:24:30 -0300

    adicionado o esqueleto do projeto

---
+++ src/main/java/com/yandaniel/bff_agendador_tarefas/BffAgendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.yandaniel.bff_agendador_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yandaniel/bff_agendador_tarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yandaniel.bff_agendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
